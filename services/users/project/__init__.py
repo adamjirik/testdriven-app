4aa4a97827a87859a7e15c5299219e6ace8226e8
Adam Jirik <adam.jirik@edgenpd.com>
2019-03-12 21:55:00 +0100

    post route for user finished

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,12 +1,29 @@
-from flask import Flask, jsonify
+import os
 
-app = Flask(__name__)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app.config.from_object('project.config.DevelopmentConfig')
+db = SQLAlchemy()
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-	return jsonify({
-		'status': 'success',
-		'message': 'pong!'
-		})
+def create_app(script_info=None):
+
+	#instatiate app
+	app = Flask(__name__)
+
+	#set config
+	app_settings = os.getenv('APP_SETTINGS')
+	app.config.from_object(app_settings)
+
+	#set up exensions
+	db.init_app(app)
+
+	#register blueprints
+	from project.api.users import users_blueprint
+	app.register_blueprint(users_blueprint)
+
+	#shell context for flask cli
+	@app.shell_context_processor
+	def ctx():
+		return dict(app=app, db=db)
+
+	return app
