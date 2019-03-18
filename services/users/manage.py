07222f56b9f63cdf42ec34f3b86b6a4bba159847
Adam Jirik <adam.jirik@edgenpd.com>
2019-03-18 11:52:14 +0100

    part 2 chapter 3 travis and coverage added

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,10 +1,20 @@
 import unittest
 
+import coverage
 from flask.cli import FlaskGroup
 
 from project import db, create_app
 from project.api.models import User
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py'
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -31,6 +41,21 @@ def seed_db():
     db.session.add(User(username="adam", email="adam@dev.com"))
     db.session.add(User(username="adamj", email="adamj@dev.com"))
     db.session.commit()
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage"""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
