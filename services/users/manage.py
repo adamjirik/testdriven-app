1939a9e39f1c6448af0cacf4e8feb33fae6e0eb8
Adam Jirik <adam.jirik@edgenpd.com>
2019-03-21 16:11:33 +0100

    auth status view added, end of part three auth routes

--- services/users/manage.py
+++ services/users/manage.py
@@ -44,8 +44,8 @@ def test():
 @cli.command('seed_db')
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username='michael', email="hermanmu@gmail.com", password='morethaneight'))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org", password='morethaneight'))
     db.session.commit()
 
 
